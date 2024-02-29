ca46dc4b4c4668d2292a1c15403a326174739a03
Kerryk930 <91487021+Kerryk930@users.noreply.github.com>
2024-02-28 21:50:27 -0500

    Add files via upload

---
+++ Term Project/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+app.use(express.json()); 
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const data = require('./data.json');
+
+app.get('/posts', (req, res) => {
+    res.json(data.posts);
+});
+
+
+app.get('/posts/:postId', (req, res) => {
+    const { postId } = req.params;
+    const post = data.posts[postId];
+    if (post) {
+        res.json({post});
+    } else {
+        res.status(404).send('Post not found');
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
